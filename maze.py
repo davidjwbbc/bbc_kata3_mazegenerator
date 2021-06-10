8ce7504e00209b3cafc440d8d87a7fc3ea2e25c4
David Waring <davidjw@rd.bbc.co.uk>
2021-06-10 17:24:24 +0100

    My version of the maze generator using python classes.

--- maze.py
+++ maze.py
@@ -1,77 +1,209 @@
+#!/usr/bin/python3
+'''
+Maze generator
+==============
+
+Generates a maze using a depth-first algorithm
+
+The main class is Maze which uses Maze.Cell objects to store information about
+cells in the maze.
+'''
+
+import random
 import sys
-from random import randint
 
-# 1 = top, 2 = left, 4 = bottom, 8 = right so 15 = untouched
+class Maze:
+  '''Maze - class to hold a maze of cells
+'''
 
-# constants to make things easier to read
-WALLS = 0
-VISITED = 1
+  def __init__(self, max_width, max_height):
+    'Constructor - create an empty maze with maximum dimensions'
+    self.__max_width = max_width
+    self.__max_height = max_height
+    self.__cellrows = {'min': [None, None], 'max': [None, None]}
 
-# global stack for iterative checks of neighbours
-stack = []
+  class Cell:
+    '''Class to hold information about an individual cell
 
-def create_maze (x, y):
-    # create initial arrays with walls = 15 and visited = false
-    m = [ [ [0b1111,0] for w in range(x) ] for h in range(y) ]
+This class can also represent an empty Cell which is not part the maze at
+present.'''
 
-    # pick a random cell as our start cell and mark it as visited and add to stack
-    (sx, sy) = [randint(0, x-1), randint(0, y-1)]
-    m[sx][sy][VISITED] = True
-    stack.append([sx, sy])
+    def __init__(self, maze, x, y, empty=False):
+      '''Constructor - create a new walled cell or empty cell
 
-    # keep going through the stack until all cells visited
-    while len(stack) > 0:
-        # take a cell off stack and get all it's neighbours
-        (sx, sy)= stack.pop()
-        neighbours = get_neighbours(sx, sy , x, y)
+Creates a Cell at the x,y position in the given maze. If the cell is currently
+empty in the maze then the walls are derived from the presence of adjacent
+cells in the maze. If this is a new non-empty cell then all walls are present.
+'''
+      self.__maze = maze
+      self.x = x
+      self.y = y
+      self.__empty = empty
+      if empty:
+        self.__wall_below = maze.cellExists(x, y+1)
+        self.__wall_right = maze.cellExists(x+1, y)
+      else:
+        self.__wall_below = True
+        self.__wall_right = True
 
-        for (nx, ny) in neighbours:
-            if m[nx][ny][VISITED] == False:
-                stack.append([sx, sy])
+    def __str__(self):
+      '''Return a string representation of right and bottom walls (left and top
+are handled by adjacent cells).'''
+      ret = ''
+      if self.__wall_below:
+        ret += '_'
+      else:
+        ret += ' '
+      if self.__wall_right:
+        ret += '|'
+      else:
+        if self.__wall_below:
+          ret += '_'
+        else:
+          ret += ' '
+      return ret
 
-                #pick a random neighbour
-                (nx, ny) = neighbours[randint(0, len(neighbours)-1)]
+    def __repr__(self):
+      '''Return a string representation of this cell for debugging. Mark as
+Transient if this cell object was created as a temporary empty maze cell.'''
+      return 'Maze.%sCell(x=%i, y=%i, walls="%s")'%({False:'', True: 'Transient'}[self.__empty], self.x, self.y, str(self))
 
-                # remove wall if not visited
-                if m[nx][ny][VISITED] == False:
-                    m[nx][ny][VISITED] = True
-                    if nx > sx: m[ny][nx][WALLS] = m[ny][nx][WALLS] - 2
-                    if nx < sx: m[sy][sx][WALLS] = m[sy][sx][WALLS] - 2
-                    if ny > sy: m[ny][nx][WALLS] = m[ny][nx][WALLS] - 1
-                    if ny < sy: m[sy][sx][WALLS] = m[sy][sx][WALLS] - 1
+    def hasWallRight(self):
+      '''Return True if there is a righthand wall to this cell'''
+      return self.__wall_right
 
-                stack.append([nx, ny])
-                m[nx][ny][VISITED] = True
-    return m
+    def hasWallBelow(self):
+      '''Return True if there is a bottom wall to this cell'''
+      return self.__wall_below
 
-def get_neighbours(x, y, mx, my):
-    # return a list of valid neighbours (ie nothing outside the maze)
-    n = []
-    if x > 0: n.append((x-1, y))
-    if y > 0: n.append((x, y-1))
-    if x < mx-1: n.append((x+1, y))
-    if y < my-1: n.append((x, y+1))
-    return n
-    
-def print_maze (m):
-    for row in m:
-        for cell in row:
-            if cell[WALLS] & 0b1 : print ('+--', end='')
-            else: print ('+  ', end='')
-        if cell[WALLS] & 0b1000: print ('+', end='') # last column
-        print('')
+    def hasWallLeft(self):
+      '''Return True if there is a lefthand wall to this cell'''
+      return self.__maze.getcell(self.x-1, self.y).hasWallRight()
 
-        for cell in row:
-            if cell[WALLS] & 0b10 : print ('|  ', end ='')
-            else: print ('   ', end ='')
-        if cell[WALLS] & 0b1000: print ('|', end='') # last column
-        print('')
+    def hasWallAbove(self):
+      '''Return True if there is a top wall to this cell'''
+      return self.__maze.getcell(self.x, self.y-1).hasWallBelow()
 
-    #last row
-    for cell in row:
-        if cell[WALLS] & 0b100 : print ('+--',end ='')
-    if cell[WALLS] & 0b100: print ('+', end='') # last column
-    print('')
+    def breakWallRight(self):
+      '''Remove the righthand wall of this cell. Return True if there was a wall to remove.'''
+      if not self.__empty and self.__wall_right:
+        self.__wall_right = False
+        return True
+      return False
 
-m = create_maze(int(sys.argv[1]),int(sys.argv[2]))
-print_maze(m)
+    def breakWallBelow(self):
+      '''Remove the bottom wall of this cell. Return True if there was a wall to remove.'''
+      if not self.__empty and self.__wall_below:
+        self.__wall_below = False
+        return True
+      return False
+
+    def breakWallLeft(self):
+      '''Remove the lefthand wall of this cell. Return True if there was a wall to remove.'''
+      return self.__maze.getcell(self.x-1, self.y).breakWallRight()
+
+    def breakWallAbove(self):
+      '''Remove the top wall of this cell. Return True if there was a wall to remove.'''
+      return self.__maze.getcell(self.x, self.y-1).breakWallAbove()
+
+  def createCell(self, x, y):
+    '''Maze.createCell - Create a cell in the maze if within bounds.
+
+Will create a new cell or return an existing cell if x,y is a valid position
+in the maze.
+
+Returns the Maze.Cell at x,y in the Maze or None if x,y is out of bounds.'''
+    if x < 0 or x >= self.__max_width:
+      return None
+    if y < 0 or y >= self.__max_height:
+      return None
+    if self.__cellrows['min'][1] is None or self.__cellrows['min'][1] > y:
+      self.__cellrows['min'][1] = y
+    if self.__cellrows['max'][1] is None or self.__cellrows['max'][1] < y:
+      self.__cellrows['max'][1] = y
+    if self.__cellrows['min'][0] is None or self.__cellrows['min'][0] > x:
+      self.__cellrows['min'][0] = x
+    if self.__cellrows['max'][0] is None or self.__cellrows['max'][0] < x:
+      self.__cellrows['max'][0] = x
+    if y not in self.__cellrows:
+      ret = Maze.Cell(self, x, y)
+      self.__cellrows[y] = {x: ret}
+      return ret
+    row = self.__cellrows[y]
+    if x not in row:
+      ret = Maze.Cell(self, x, y)
+      row[x] = ret
+      return ret
+    return row[x]
+
+  def _recursivePopulate(self, cell):
+    '''Recursive maze population function'''
+    for dir in random.sample([((cell.x-1, cell.y), lambda x: x.breakWallRight()),
+                ((cell.x, cell.y-1), lambda x: x.breakWallBelow()),
+                ((cell.x+1, cell.y), lambda x: cell.breakWallRight()),
+                ((cell.x, cell.y+1), lambda x: cell.breakWallBelow())], 4):
+      if not self.cellExists(*dir[0]):
+        nc = self.createCell(*dir[0])
+        if nc is not None:
+          dir[1](nc)
+          self._recursivePopulate(nc)
+
+  def populateFrom(self, x, y):
+    '''Populate the entire maze using a depth-first algorithm starting at x,y.
+'''
+    if self.cellExists(x, y):
+      return False
+    cell = self.createCell(x, y)
+    self._recursivePopulate(cell)
+    return True
+
+  def cellExists(self, x, y):
+    '''Return True if a Maze.Cell exists at position x,y in the Maze.'''
+    if y not in self.__cellrows:
+      return False
+    if x not in self.__cellrows[y]:
+      return False
+    return True
+
+  def getCell(self, x, y):
+    '''Get an existing or empty Maze.Cell for position x,y.'''
+    if x < 0 or x >= self.__max_width:
+      return Maze.Cell(self, x, y, empty=True)
+    if y < 0 or y >= self.__max_height:
+      return Maze.Cell(self, x, y, empty=True)
+    if y not in self.__cellrows:
+      return Maze.Cell(self, x, y, empty=True)
+    if x not in self.__cellrows[y]:
+      return Maze.Cell(self, x, y, empty=True)
+    return self.__cellrows[y][x]
+
+  def __str__(self):
+    '''Return a string representation of the Maze.'''
+    ret = ''
+    # Need to iterate through the range of cells from one to the left of the
+    # leftmost defined cell, and for the row above the top row of defined
+    # cells in order to get empty cells to fill in the missing top and left
+    # walls.
+    for row in range(self.__cellrows['min'][1]-1, self.__cellrows['max'][1]+1):
+      for col in range(self.__cellrows['min'][0]-1, self.__cellrows['max'][0]+1):
+        cell = self.getCell(col,row)
+        ret += str(cell)
+      ret += '\n'
+    return ret
+
+def main():
+  '''Main function if running as a standalone program.'''
+  max_width = 30
+  max_height = 30
+  if len(sys.argv) > 1:
+    max_width = int(sys.argv[1])
+  if len(sys.argv) > 2:
+    max_height = int(sys.argv[2])
+  m = Maze(max_width, max_height)
+  m.populateFrom(random.randint(0, max_width-1),random.randint(0, max_height-1))
+  print(str(m))
+  return 0
+
+if __name__ == '__main__':
+  '''If run as a program, trigger main().'''
+  sys.exit(main())
